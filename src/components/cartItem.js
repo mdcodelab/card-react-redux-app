d6ea83622c8f9d29e02340df4cb6e09ce2e37d72
miha11-hash <d_mihaela@msn.com>
2023-04-14 12:31:56 +0300

    CartItem

--- src/components/cartItem.js
+++ src/components/cartItem.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { ChevronUp, ChevronDown } from '../icons';
 
-function cartItem() {
+
+function cartItem({id, img, title, price, amount}) {
   return (
-    <div>
-      cartItem
-    </div>
+    <article className="cart-item">
+      <img src={img} alt={title}></img>
+      <div>
+        <h4>{title}</h4>
+        <h4 className="item-price">${price}</h4>
+        <button className="remove-btn">Remove</button>
+      </div>
+      <div>
+        <button className="amount-btn"><ChevronUp></ChevronUp></button>
+        <p className="amount">{amount}</p>
+        <button className="amount-btn"><ChevronDown></ChevronDown></button>
+      </div>
+    </article>
   );
 }
 
